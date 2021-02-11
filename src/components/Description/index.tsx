f2597120bb7fc8ac73b82774d93011481de07640
Ryosuke <hello.ryosuke.u@gmail.com>
2021-02-11 13:32:21 +0900

    概要を多言語対応

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
+import '../../i18n';
+import { useTranslation } from 'react-i18next';
 
-const Description : React.FC = () => (
-  <div className="content">
-    <p>
-      JJUG CCCは毎年2回、春と秋に開催する日本最大のJavaコミュニティイベントです。<br/>
-      Java関連の技術や事例に関する良質なセッションが行われ、また異なる分野で活躍するJava技術者が一堂に会する場ともなっています。
-    </p>
+const Description : React.FC = () => {
+  const { t, i18n } = useTranslation();
+
+  return <div className="content">
+    <p>{t("description.text")}</p>
 
     <dl>
-      <dt>主催</dt>
-      <dd>日本Javaユーザグループ</dd>
-      <dt>開催日時</dt>
-      <dd>2021年05月23日（日）</dd>
-      <dt>参加費</dt>
-      <dd>無料</dd>
-      <dt>登録</dt>
-      <dd><a href="https://jjug.doorkeeper.jp/events/112842" target="blank"
-             rel="noopener">https://jjug.doorkeeper.jp/events/112842</a></dd>
-      <dt>スケジュール</dt>
-      <dd><a href="https://confengine.com/jjug-ccc-2020-fall/schedule/rich" target="blank"
-             rel="noopener">https://confengine.com/jjug-ccc-2020-fall/schedule/rich</a></dd>
-      <dt>ハッシュタグ</dt>
-      <dd><a href="https://twitter.com/search?q=%23jjug_ccc" target="blank" rel="noopener">#jjug_ccc</a>（セッションごとのハッシュタグあり）
-      </dd>
-      <dt>開催方法</dt>
-      <dd>オンライン</dd>
+      <dt>{t("description.title.organizer")}</dt>
+      <dd>{t("description.data.organizer")}</dd>
+      <dt>{t("description.title.date")}</dt>
+      <dd>{t("description.data.date")}</dd>
+      <dt>{t("description.title.participationFee")}</dt>
+      <dd>{t("description.data.participationFee")}</dd>
+      <dt>{t("description.title.register")}</dt>
+      <dd><a href="https://jjug.doorkeeper.jp/events/112842" target="blank" rel="noopener">https://jjug.doorkeeper.jp/events/112842</a></dd>
+      <dt>{t("description.title.schedule")}</dt>
+      <dd><a href="https://confengine.com/jjug-ccc-2020-fall/schedule/rich" target="blank" rel="noopener">https://confengine.com/jjug-ccc-2020-fall/schedule/rich</a></dd>
+      <dt>{t("description.title.hashtag")}</dt>
+      <dd><a href="https://twitter.com/search?q=%23jjug_ccc" target="blank" rel="noopener">#jjug_ccc</a>（{t("description.data.hashtag")}）</dd>
     </dl>
   </div>
-);
+};
 
 export default Description;
