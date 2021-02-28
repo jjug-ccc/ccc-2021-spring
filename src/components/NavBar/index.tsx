5dc26a61d1c29b22053f02b9f619321d778528a8
Ryosuke <hello.ryosuke.u@gmail.com>
2021-02-28 20:17:26 +0900

    登録ボタンを削除

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -13,18 +13,6 @@ const NavBar : React.FC = () => {
       <div className="navbar-brand">
         <img className="navbar-item" src={jjug_logo} alt="jjug logo"/>
       </div>
-
-      <div className="navbar-menu">
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-danger" target="_blank" rel="noopener noreferrer" href={LINK.register}>
-                <strong>{t("nav.registerBtn")}</strong>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
     </nav>
   );
 };
