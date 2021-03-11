96b03e84c234f0f7b322c6f26423fcfeb4bb2ba7
Ryosuke <hello.ryosuke.u@gmail.com>
2021-03-12 00:50:15 +0900

    利用してない箇所削除

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import '../../assets/styles.scss';
 import jjug_logo from '../../static/icons/jjug_logo.png';
-import { LINK } from '../../constant';
 import '../../i18n';
-import { useTranslation } from 'react-i18next';
 
 const NavBar : React.FC = () => {
-  const { t, i18n } = useTranslation();
-
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
