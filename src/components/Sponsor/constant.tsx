9a7dee165df945ed9fa39c26a842e5bd7725f7c7
Ryosuke <hello.ryosuke.u@gmail.com>
2021-04-29 12:17:03 +0900

    スポンサー一覧追加

--- src/components/Sponsor/constant.tsx
+++ src/components/Sponsor/constant.tsx
@@ -1,6 +1,30 @@
-export type SponsorProps = {
-  image: string,
-  alt: string,
-  url: string,
-};
+import { SponsorLogoProps, SponsorType } from './index';
 
+export const SPONSORS = new Map<
+  SponsorType,
+  Array<SponsorLogoProps>
+>([
+    [ 'Gold',
+      [
+        { image: 'casareal.png', alt: '株式会社カサレアル', url: '' },
+        { image: 'microsoft.png', alt: '日本マイクロソフト株式会社', url: '' },
+        { image: 'line.png', alt: 'LINE株式会社', url: '' },
+        // { image: 'gmo_internet.png', alt: 'GMOインターネット株式会社', url: '' },
+        // { image: 'recruit.png', alt: '株式会社リクルート', url: '' },
+        // { image: 'azul_systems.png', alt: 'Azul Systems', url: '' },
+        { image: 'jfrog.png', alt: 'JFrog', url: '' },
+        { image: 'techmatrix.png', alt: 'テクマトリックス株式会社', url: '' },
+        { image: 'ulsystems.png', alt: 'ウルシステムズ株式会社', url: '' },
+        { image: 'redhat.png', alt: 'レッドハット株式会社', url: '' },
+      ]
+    ],
+    [
+      'Silver',
+      [
+        { image: 'rakusu.png', alt: '株式会社ラクス', url: '' },
+        { image: 'softbank.png', alt: 'ソフトバンク株式会社', url: '' },
+        { image: 'm3.png', alt: 'エムスリー株式会社', url: '' }
+      ]
+    ]
+  ]
+);
--- src/components/Sponsor/index.tsx
+++ src/components/Sponsor/index.tsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { SPONSORS } from './constant';
 
 export type SponsorProps = {
+  sponsorType: SponsorType,
+};
+
+export type SponsorLogoProps = {
   image: string,
   alt: string,
   url: string,
 };
 
-const Sponsor: React.FC<Array<SponsorProps>> = (sponsors: Array<SponsorProps>) => (
-  <div className='columns is-multiline is-mobile'>
-    {sponsors.map(sponsor => (
-      <div key={sponsor.image} className='column is-half-mobile is-one-quarter-desktop' style={{ borderRadius: '5px' }}>
-        <div>
-          <a href={sponsor.url} target="_blank" rel="noopener noreferrer"><img alt={sponsor.alt} src={sponsor.image}/></a>
-        </div>
-      </div>
-    ))}
-  </div>
-);
+export type SponsorType = 'Gold' | 'Silver';
+
+const Sponsor: React.FC<SponsorProps> = ({sponsorType}) => {
+  const sponsors = SPONSORS.get(sponsorType);
+  if (sponsors === undefined) {
+    return (<div className="columns is-multiline is-mobile"/>);
+  }
+
+  return (
+    <div className="columns is-multiline is-mobile">
+      {
+        sponsors.map(
+          sponsor =>
+            <div key={sponsor.image} className="column is-half-mobile is-one-quarter-desktop" style={{ borderRadius: '5px' }}>
+              <div>
+                <a href={sponsor.url} target="_blank" rel="noopener noreferrer"><img alt={sponsor.alt} src={'/static/images/sponsor/' + sponsor.image}/></a>
+              </div>
+            </div>
+        )
+      }
+    </div>
+  );
+};
 
 export default Sponsor
